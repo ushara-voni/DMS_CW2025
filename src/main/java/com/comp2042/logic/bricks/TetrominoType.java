3a7aa2ab433a8a2de0c04e37fceec67d84bc6e08
ushara-voni <usharafernando376@gmail.com>
2025-11-21 10:19:55 +0800

    Change colours and look of the user interface. Added css files for main menu and game over.

--- src/main/java/com/comp2042/logic/bricks/TetrominoType.java
+++ src/main/java/com/comp2042/logic/bricks/TetrominoType.java
@@ -3,13 +3,13 @@ package com.comp2042.logic.bricks;
 import javafx.scene.paint.Color;
 
 public enum TetrominoType {
-    I(Color.AQUA),
-    O(Color.YELLOW),
-    T(Color.BLUEVIOLET),
-    S(Color.DARKGREEN),
-    Z(Color.RED),
-    J(Color.BEIGE),
-    L(Color.BURLYWOOD);
+    I(Color.web("#A0F0FF")),   // pastel cyan
+    O(Color.web("#FFF4A3")),   // pastel yellow
+    T(Color.web("#D8B0FF")),   // lavender
+    S(Color.web("#B7FFCC")),   // mint green
+    Z(Color.web("#FFB0B0")),   // soft pink
+    J(Color.web("#FFD1A8")),   // peach
+    L(Color.web("#E3C8FF"));   // light lilac
 
     private final Color color;
 
@@ -24,5 +24,4 @@ public enum TetrominoType {
     public int getId() {
         return this.ordinal() + 1;
     }
-
 }
